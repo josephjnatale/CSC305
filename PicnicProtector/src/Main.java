ab3330af9eaafa3357a6fab95842872dafc28813
Joe Natale <Josephjnatale@gmail.com>
2016-06-07 22:58:01 -0400

    created a project with the base outline of the project

---
+++ PicnicProtector/src/Main.java
@@ -0,0 +1,14 @@
+import processing.core.PApplet;
+public class Main extends PApplet {
+	
+	public void setup(){
+		size(500,500);
+		background(255);
+		
+	}
+	
+	public void draw(){
+		fill(150,150,2);
+		rect(0,0,50,50);
+	}
+}
